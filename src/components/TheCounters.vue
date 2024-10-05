d913a79d76d08bcc80b7d0697c5cc22ff5485e12
fadesyphe <pivkin.jura2017@gmail.com>
2024-10-05 22:47:06 +0600

    Created TheCounters component

---
+++ src/components/TheCounters.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center gap-[120px] mt-16 w-[100vw] max-w-[1440px] bg-base py-24 rounded-[32px] px-[66px]">
+    <div class="">
+      <h3 class="font-jost font-semibold text-[40px] text-primary">12.000 +</h3>
+      <span class="text-content text-xl">Downloaded</span>
+    </div>
+    <div class="">
+      <h3 class="font-jost font-semibold text-[40px] text-primary">$10 M</h3>
+      <span class="text-content text-xl">Transactions</span>
+    </div>
+    <div class="">
+      <h3 class="font-jost font-semibold text-[40px] text-primary">1.000 +</h3>
+      <span class="text-content text-xl">Feedback</span>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+
+export default {
+
+}
+</script>
+
--- src/components/ThePayments.vue
+++ src/components/ThePayments.vue
@@ -20,20 +20,7 @@
             </p>
         </div>
       </div>
-      <div class="flex items-center gap-[120px] mt-16 w-[100vw] max-w-[1440px] bg-base py-24 rounded-[32px] px-[66px]">
-        <div class="">
-          <h3 class="font-jost font-semibold text-[40px] text-primary">12.000 +</h3>
-          <span class="text-content text-xl">Downloaded</span>
-        </div>
-        <div class="">
-          <h3 class="font-jost font-semibold text-[40px] text-primary">$10 M</h3>
-          <span class="text-content text-xl">Transactions</span>
-        </div>
-        <div class="">
-          <h3 class="font-jost font-semibold text-[40px] text-primary">1.000 +</h3>
-          <span class="text-content text-xl">Feedback</span>
-        </div>
-      </div>
+      <TheCounters />
     </div>
       <img src="../assets/images/background-payments.png" alt="">
     </div>
@@ -43,10 +30,12 @@
 
 <script>
 import BaseIcon from "./BaseIcon.vue";
+import TheCounters from "./TheCounters.vue"
 
 export default {
   components: {
-    BaseIcon
+    BaseIcon,
+    TheCounters
   }
 }
 </script>
